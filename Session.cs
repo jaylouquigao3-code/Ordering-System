0513569cefa177c619026fd0df78f6ab652b04d3
Jaylou Quigao <jaylouquigao3@gmail.com>
2026-05-19 05:30:00 +0800

    Add session mananagement

--- Session.cs
+++ Session.cs
@@ -1,9 +1,11 @@
 ﻿namespace Ordering_System
 {
+
     public static class Session
     {
         public static int customer_id;
 
         public static string fullname;
     }
+
 }
--- obj/Debug/net8.0-windows/Ordering System.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Ordering System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ordering System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35303e046070ec466613201460d13646f45ebe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ordering System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ordering System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
